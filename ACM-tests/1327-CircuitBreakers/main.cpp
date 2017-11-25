4a91c3553a0906f80947dcd2b8710ebfe322a42e
Mark Vodyanitskiy <mvodya@gmail.com>
2017-11-25 13:24:42 +1000

    1327 test added

---
+++ ACM-tests/1327-CircuitBreakers/main.cpp
@@ -0,0 +1,21 @@
+﻿// http://acm.timus.ru/problem.aspx?space=1&num=1327
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+	// Ввод данных
+	scanf_s("%d %d", &a, &b);
+
+	// Вычисляем
+	c = (b - a) + (a % 2) + (b % 2);
+	c /= 2;
+
+	// Ответ
+	printf("%d", c);
+
+	system("pause");
+	return 0;
+}
